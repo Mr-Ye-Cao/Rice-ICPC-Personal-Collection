4b37ab2d93a283d455de6ddd7b582322bf8a2f1a
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2020-11-25 04:09:07 -0600

    Create add_candies.cpp

---
+++ Problem/add_candies.cpp
@@ -0,0 +1,108 @@
+/***
+  $800 Math
+  https://codeforces.com/problemset/problem/1447/A
+  Add a number c to all the numbers except number A is equivalent to deducting c
+  from number A.
+***/
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;cin>>n;
+		cout<<n<<endl;
+		for(int i=1;i<=n;i++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
