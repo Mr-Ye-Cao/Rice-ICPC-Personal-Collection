172981b382e212f3f3c0a9a8d650c79c4742b751
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2020-11-26 02:33:28 -0600

    Create elimination.cpp

---
+++ Problem/elimination.cpp
@@ -0,0 +1,114 @@
+/***
+  $900 Math
+  https://codeforces.com/problemset/problem/1445/B
+  I did not completely understand the problem
+  but the problem is solved using some kind of mathematical
+  structure that I guessed which will produce the right output
+***/
+
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		int fi=min(a,d);
+		int se=max(b,c);
+		int c1=fi+se;
+		fi=max(a,d);
+		se=min(b,c);
+		int c2=fi+se;
+		cout<<max(c1,c2)<<endl;
+	}
+	
+	return 0;
+}
