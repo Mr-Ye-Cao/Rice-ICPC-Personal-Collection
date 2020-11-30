fccacf644269e1625c9ba9772ed0c997dc82bf70
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2020-11-30 06:40:52 -0600

    Create radio_tower.cpp

---
+++ Problem/radio_tower.cpp
@@ -0,0 +1,141 @@
+/***
+  $1600 Problem Solving Module_arithmatic
+  Had I thought more generically and less about how to distribute the radio signal but more on the
+  numerical patern of the answer. I would be more likely to solve it.
+  As it turned out, the patern is very beautifully a fibbonaci sequence.
+  One take away from this problem is how to do Module_arithmatic for large numbers.
+***/
+
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+const int MOD = 998244353;
+
+int add(int x, int y)
+{
+    x += y;
+    while(x >= MOD) x -= MOD;
+    while(x < 0) x += MOD;
+    return x;
+}
+
+int mul(int x, int y)
+{
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y)
+{
+    int ans = 1;
+    while(y > 0)
+    {
+        if(y % 2 == 1)
+            ans = mul(ans, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return ans;
+}
+
+int divide(int x, int y)
+{
+    return mul(x, binpow(y, MOD - 2));
+}
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> fib(n + 1);
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i <= n; i++)
+        fib[i] = add(fib[i - 1], fib[i - 2]);
+    cout << divide(fib[n], binpow(2, n)) << endl;
+
+	return 0;
+}
