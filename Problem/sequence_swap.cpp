a27cfab3c517132f11450ad26db048201519fd91
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2020-12-06 21:31:50 +0800

    Update sequence_swap.cpp

--- Problem/sequence_swap.cpp
+++ Problem/sequence_swap.cpp
@@ -3,6 +3,11 @@
   https://codeforces.com/problemset/problem/1455/D
   The subproblem I think is to just always greedily solve the
   very first conflict. Still some bugs need to be fixed.
+  
+  Correction: subproblem is to always first solve the minimum conflict 
+  among the candidate conflicts. If x is smaller than the minimum conflict
+  then it would be impossible to sovle it. This is proven by drawing chart and reach an contradiction.
+  	     
 
 ***/
 
@@ -98,48 +103,49 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-		int n,x,cost=0;
+		int n,x;
 		cin>>n>>x;
 
 		vector<int> a(n);
 		for(int i=0;i<n;i++)
 			cin>>a[i];
 		
-		vector<int> c;
-		for(int j=n-2;j>=0;j--)
+		vector<pair<int,int> > c;
+		for(int j=0;j<n-1;j++)
 			if(a[j]>a[j+1])
-				c.push_back(j);
-			
+				c.push_back(make_pair(a[j],j));	
+
+		sort(c.begin(),c.end());
+		if(c.size()!=0&&c[0].first<x){
+			cout<<"-1"<<endl;
+			continue;
+		}
+		reverse(c.begin(),c.end());
+
+		int cost=0;
 		bool imp=false;
 		while(c.size()!=0){
-			int ind=c.back();
+			pair<int,int> f_c=c.back();
+			int con_num=f_c.first;
+			int con_ind=f_c.second;
 			c.pop_back();
-			vector<int>::iterator low=lower_bound(a.begin(),a.begin()+ind,x);
-			// cout<<"LOW "<<low-a.begin()<<" "<<x<<endl;
-			if(low==a.begin()+ind&&ind!=0&&c.size()!=0){
-				cout<<"-1"<<endl;
+
+			vector<int>::iterator low=lower_bound(a.begin(),a.begin()+con_ind+1,x);
+			//low is to insert x
+			a.erase(a.begin()+con_ind);
+			a.insert(low,x);
+			if(a[con_ind]>a[con_ind+1]){
 				imp=true;
+				cout<<"-1"<<endl;
 				break;
-			}else{
-				if(a[ind+1]<x){
-					cout<<"-1"<<endl;
-					imp=true;
-					break;
-				}
-				int nex=a[ind];
-				a.erase(a.begin()+ind);
-				a.insert(low,x);
-				x=nex;
-				cost+=(ind-(low-a.begin())+1);
 			}
-			// for(int j=0;j<a.size();j++){
-			// 	cout<<a[j]<<" ";
-			// }
-				// cout<<endl;
+			x=con_num;
+			cost+=(a.begin()+con_ind-low)+1;
 		}
-		if(!imp){
+
+		if(!imp)
 			cout<<cost<<endl;
-		}
+
 	}
 
 	return 0;
