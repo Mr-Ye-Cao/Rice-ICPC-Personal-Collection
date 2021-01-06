c458993786c7af9582a8c9506cb76cd7d66e2228
Mr-Ye-Cao <60802056+Mr-Ye-Cao@users.noreply.github.com>
2021-01-06 01:42:37 -0600

    Create card_friends.cpp

---
+++ Problem/card_friends.cpp
@@ -0,0 +1,123 @@
+/***
+https://codeforces.com/contest/1472/problem/A
+***/
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+ 
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+ 
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+typedef long long ll;
+ 
+ 
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		ll w,h,n;
+		cin>>w>>h>>n;
+		ll cw=0,ch=0;
+
+		while(w%2==0){
+			w=w>>1;
+			cw++;
+		}
+
+		while(h%2==0){
+			h=h>>1;
+			ch++;
+		}
+
+		ll res=(1<<cw)*(1<<ch);
+		if(res>=n)
+			cout<<"YES"<<endl;
+		else
+			cout<<"NO"<<endl;
+
+		// cout<<res<<endl;
+	}
+
+    return 0;
+}
